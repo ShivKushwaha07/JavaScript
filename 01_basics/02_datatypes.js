8bb08c321da22bdaf79c982ca5003062a1142dd7
Shiv Kumar Kushwaha <90277241+ShivKushwaha07@users.noreply.github.com>
2023-01-06 04:44:47 +0000

    datatype.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert("3 + 3") it is work on browser not node js and we are using node js.
+
+console.log(3 +3)
+// code readability shoiuld high
+console.log("Shiv")
+
+let name = "Shiv"
+let age = 24
+let isloggedin = false
+let state;
+//premetive data types
+// number => 2 to power 53
+//if number is very big then use bigint
+//string => ""
+//boolear => true, false
+//nul => standalone value
+//undefined =>  
+// synbol => it use for unique identify
+
+
+//non-premetive data types
+// object =>
+
+console.log(typeof "hello");// this is string type
+console.log(typeof null);
+console.log(typeof undefined);
+// null is a object
+
