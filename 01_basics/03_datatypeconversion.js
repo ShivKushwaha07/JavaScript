e982c5e9015ea58f00c8f5acc39e0e840e0f90fa
Shiv Kumar Kushwaha <90277241+ShivKushwaha07@users.noreply.github.com>
2023-01-07 05:18:22 +0000

    conversion of datatype

---
+++ 01_basics/03_datatypeconversion.js
@@ -0,0 +1,28 @@
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+
+// "33" => 33
+// "3abc" => Nan
+// true => 1; false = 0
+
+// let isloggedIn = 1
+// let isloggedIn = ""
+
+// let booleanIsloggedIn = Boolean(isloggedIn)
+// console.log(booleanIsloggedIn);
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+// let valueInnumber = Number(score)
+//console.log(typeof valueInnumber);
+//console.log(valueInnumber);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
